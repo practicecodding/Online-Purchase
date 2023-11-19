ce3425ff6452da0a6442faa53e246d0692b96875
user <practicecoddin@gmail.com>
2023-11-19 08:10:43 +0600

    Fragment,Internet Connect,DataBase Connect,Data Parse From Data Base

--- app/src/main/java/com/hamidul/onlinepurchase/CartList.java
+++ app/src/main/java/com/hamidul/onlinepurchase/CartList.java
@@ -3,6 +3,7 @@ package com.hamidul.onlinepurchase;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.health.connect.datatypes.StepsCadenceRecord;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,10 +13,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -26,11 +32,14 @@ public class CartList extends Fragment {
     ArrayList<ModelClass> cartList = new ArrayList<>();
     SharedPreferences sharedPreferences;
     GridView gridView;
+    LinearLayout full,empty;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_cart_list, container, false);
 
         gridView = myView.findViewById(R.id.gridView);
+        full = myView.findViewById(R.id.full);
+        empty = myView.findViewById(R.id.empty);
         sharedPreferences = getActivity().getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
 
         loadCartData();
@@ -67,14 +76,36 @@ public class CartList extends Fragment {
             TextView tvName = cartView.findViewById(R.id.tvName);
             TextView tvWeight = cartView.findViewById(R.id.tvWeight);
             TextView tvPrice = cartView.findViewById(R.id.tvPrice);
+            LinearLayout addCart = cartView.findViewById(R.id.addCart);
+            ImageView imageView = cartView.findViewById(R.id.imageView);
+            LottieAnimationView lottieAnimationView = cartView.findViewById(R.id.lottieAnimationView);
 
+            addCart.setVisibility(View.GONE);
+
+            String image = cartList.get(position).image;
             String name = cartList.get(position).name;
             String weight = cartList.get(position).weight;
             String price = cartList.get(position).price;
 
             tvName.setText(name);
             tvWeight.setText(weight);
-            tvPrice.setText(price);
+            tvPrice.setText("BDT : "+price);
+
+            Picasso.get()
+                    .load(image)
+                    .into(imageView, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            lottieAnimationView.setVisibility(View.GONE);
+                            imageView.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onError(Exception e) {
+                            imageView.setVisibility(View.GONE);
+                            lottieAnimationView.setVisibility(View.VISIBLE);
+                        }
+                    });
 
 
             return cartView;
@@ -89,6 +120,8 @@ public class CartList extends Fragment {
         cartList = gson.fromJson(json,type);
         if (cartList==null){
             cartList = new ArrayList<>();
+            empty.setVisibility(View.VISIBLE);
+            full.setVisibility(View.GONE);
         }
 
     }
