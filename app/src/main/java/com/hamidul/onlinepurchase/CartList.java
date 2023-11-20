44a128d43f604bab21b9d63c69224b43ec00cad4
user <practicecoddin@gmail.com>
2023-11-20 06:39:43 +0600

    Fragment,Internet Connect,DataBase Connect,Data Parse From Data Base

--- app/src/main/java/com/hamidul/onlinepurchase/CartList.java
+++ app/src/main/java/com/hamidul/onlinepurchase/CartList.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CartList extends Fragment {
-
     ArrayList<ModelClass> cartList = new ArrayList<>();
     SharedPreferences sharedPreferences;
     GridView gridView;
